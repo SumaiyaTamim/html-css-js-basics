dee5bdfd703bcb4aadfc623e4fd94bb3bbea33f2
Tamim Sumaiya <tamimsumaiya2002@gmail.com>
2025-05-27 13:06:24 +0800

    testing something

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,6 @@
 const btnsEle = document.querySelectorAll('button')
 const inputEle = document.getElementById('result')
-
+console.log("Testing something")
 for(let i=0; i<btnsEle.length; i++){
     btnsEle[i].addEventListener('click', ()=>{
         const btnValue = btnsEle[i].textContent;
